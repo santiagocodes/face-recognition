d7c2a383b89b0500e01350b2dbf24537dad7f2d4
mc-santiago <mari.c.santiago@europe.com>
2020-11-02 18:41:32 +0100

    update database2

--- server/server.js
+++ server/server.js
@@ -10,15 +10,14 @@ const image = require('./controllers/image');
 
 const PORT = process.env.PORT || 3000;
 
-// const db = knex({
-//    client: 'pg',
-//    connection: {
-//       host: '127.0.0.1',
-//       user: 'postgres',
-//       password: '',
-//       database: 'face-recognition',
-//    },
-// });
+// Database Connection
+const db = knex({
+   client: 'pg',
+   connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+   },
+});
 
 // express
 const app = express();
