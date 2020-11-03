698db2f07f7b73e74942c58062f9f5472aa2d5bc
mc-santiago <mari.c.santiago@europe.com>
2020-11-03 23:00:23 +0100

    console user in register

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
    };
 
    onSubmitRegister = () => {
-      fetch('https://santiagocodes-face-recognition.herokuapp.com/register', {
+      fetch('/register', {
          method: 'post',
          headers: { 
             'Content-Type': 'application/json; charset=utf-8' 
