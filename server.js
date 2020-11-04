ce061492331a029052a17f376645daff42cd1f15
mc-santiago <mari.c.santiago@europe.com>
2020-11-04 18:23:02 +0100

    cors change

--- server.js
+++ server.js
@@ -27,20 +27,21 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // cors
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'http://santiagocodes-face-recognition.heroku.com', 'https://santiagocodes-face-recognition.heroku.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
+// const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'http://santiagocodes-face-recognition.heroku.com', 'https://santiagocodes-face-recognition.heroku.com']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable")
+//       callback(null, true)
+//     } else {
+//       console.log("Origin rejected")
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+app.use(cors())
 
 // Serve React to the browser
 const path = require('path');
@@ -68,7 +69,7 @@ app.post('/signin', (req, res) => {
 
 // register --> POST user
 // ... create new user
-app.post('/register', cors(corsOptions), (req, res) => {
+app.post('/register', (req, res) => {
    console.log('register endpoint');
    register.handleRegister(req, res, db, bcrypt);
 });
