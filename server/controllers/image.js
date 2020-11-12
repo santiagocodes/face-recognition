5d24e44a9a1e5a1a26590094e72149b4a18ca246
mc-santiago <mari.c.santiago@europe.com>
2020-11-12 17:15:39 +0100

    fix err handleImage

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -23,7 +23,9 @@ const handleImage = (req, res, db) => {
       .where('id', '=', id)
       .increment('entries', 1)
       .returning('entries')
-      .then(entries => res.entries(entries[0]))
+      .then(entries => {
+         res.json(entries[0])
+      })
       .catch(err => {
          console.log(err)
          res.status(400).json('unavailable')
