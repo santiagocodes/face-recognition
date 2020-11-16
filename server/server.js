74413b4d5dd1fb2facc6422dad45ff93168c5d52
mc-santiago <mari.c.santiago@europe.com>
2020-11-16 20:49:59 +0100

    nnnnnnnn

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 // Database Connection
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
    client: 'pg',
    connection: {
